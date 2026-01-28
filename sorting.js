120d95f4c7b93e50128acd3896f839736636b012
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-01-29 00:02:15 +0600

    practiced the sorting methods of js array

---
+++ sorting.js
@@ -0,0 +1,9 @@
+const numbers = [4, 7, 12, 8, 43, 6, 1];
+
+const numbers_asc = [...numbers].sort(function (a, b) {return a - b});
+const numbers_dsc = [...numbers].sort(function (a, b) {return b - a});
+
+console.log(numbers_asc);
+console.log(numbers_dsc);
+
+//! Check the readme file
