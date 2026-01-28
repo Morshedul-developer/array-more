614b7896277262c1036d04b483de9d6f3373d1f7
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-01-28 23:02:27 +0600

    practiced three methods of array looping

---
+++ looping.js
@@ -0,0 +1,16 @@
+const numbers = [23123,5465,54,24,5,81,5,52,84,54,826];
+
+// for (let i=0; i<numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+// let n = 0;
+
+// while(n<numbers.length) {
+//     console.log(numbers[n]);
+//     n++;
+// }
+
+for(const number of numbers) {
+    console.log(number);
+}
